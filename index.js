82fc688141ac5fb2858d74bf4b1d4d3d2d1a50b8
luish000 <luish000.lh@gmail.com>
2016-01-22 18:16:30 -0430

    push commit

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict'
+
+let path = require('path')
+let Hook = require('proton-hook')
+let _ = require('lodash')
+
+module.exports = class ModelsHook extends Hook {
+
+  constructor(app) {
+    super(app)
+  }
+
+  configure() {
+    if (!this.proton.app.models)
+      this.proton.app.models = {}
+    return true
+  }
+
+  initialize() {
+    let modelsPath = path.join(this.proton.app.path, '/models')
+    let models = require('require-all')(modelsPath)
+    _.forEach(models, Model => new Model(this.proton))
+  }
+
+}
