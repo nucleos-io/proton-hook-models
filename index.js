00e9b43ba729dad7d4a72fcddae33a40d9a8944d
luish000 <luish000.lh@gmail.com>
2016-03-03 17:47:33 -0430

    Validate the instance before add to app
     - Update version in package.json

--- index.js
+++ index.js
@@ -1,25 +1,62 @@
 'use strict'
 
-let path = require('path')
-let Quark = require('proton-quark')
-let _ = require('lodash')
+const path = require('path')
+const Quark = require('proton-quark')
+const _ = require('lodash')
+const BaseModel = require('proton-base-model')
 
-module.exports = class ModelsQuark extends Quark {
+/**
+ * @class ModelsQuark
+ * @classdesc This quark is for instance models
+ * @author Luis Hernandez
+ */
+class ModelsQuark extends Quark {
 
   constructor(app) {
     super(app)
   }
 
+  /**
+   * @override
+   * @method configure
+   * @description Ask if the proton.app.models object exist, if not exist
+   * the method create the proton.app.models object
+   * @author Luis Hernandez
+   */
   configure() {
     if (!this.proton.app.models)
       this.proton.app.models = {}
-    return true
   }
 
+  /**
+   * @override
+   * @method initialize
+   * @description instance all models of the app
+   * @author Luis Hernandez
+   */
   initialize() {
-    let modelsPath = path.join(this.proton.app.path, '/models')
-    let models = require('require-all')(modelsPath)
-    _.forEach(models, Model => new Model(this.proton))
+    _.forEach(this._models, (Model, fileName) => {
+      const model = new Model(this.proton)
+      if (!(model instanceof BaseModel)) {
+        throw new Error('This class must exteds from the BaseModel class.')
+      }
+      model.fileName = fileName
+      return model
+    })
+  }
+
+  /**
+   * @method controllers
+   * @description This method get the export value of each policy present
+   * in the policies folder
+   * @author Luis Hernandez
+   * @return {Array} - All policies exported values as an array
+   */
+  get _models() {
+    const modelsPath = path.join(this.proton.app.path, '/models')
+    return require('require-all')(modelsPath)
   }
 
 }
+
+module.exports = ModelsQuark
